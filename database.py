eed608d19f575542d4c6addce3518bc9a516d9af
alexocegueda <alexocegueda59@gmail.com>
2022-03-26 02:30:43 -0500

    Added function to search database - only id works

--- database.py
+++ database.py
@@ -16,6 +16,43 @@ __program_name__ = "Al-ex Grocery Mart"
 __author__ = 'Alex Ocegueda'
 __version__ = '1.0'
 
+def search_customers_table(name=None, id=None):
+    """
+    
+    This will search for a customer by their name or by their unique primary key.
+    If the name or key is not found, it will return an error message.
+    
+    format: search_customers_table(name, id)  -> make sure to put 'None' for name or id if you don't want to search by name or id
+    
+    """
+    if name != None:
+        cur.execute("SELECT * FROM customers WHERE full_name= ?", (name,))
+
+        if(cur.fetchone() == None):
+            print("Customer not found")
+
+    elif id != None:
+        cur.execute("SELECT * FROM customers WHERE rowid = ?", (id,))
+    else:
+        print("Error - Please enter a name or id to search for.")
+        return
+
+    for customer in cur.fetchall():
+        print(customer)
+
+def display_customers_with_id():
+    """
+    
+    Displays the customer with their unique primary key.
+
+    """
+    cur.execute("SELECT rowid, * FROM customers")  # Retrieves everything from customers table
+
+    for customer in cur.fetchall():
+        full_name = customer[0]
+        email = customer[1]
+
+        print(f"{full_name} \t - \t {email}")
 
 def display_customers_table():
     """
@@ -50,9 +87,9 @@ def add_new_customer():
 
 
 def main():
- 
-    display_customers_table()
-
+    
+    search_customers_table(name='Jane Doe')
+    
     con.commit()  # Commit changes in database
 
     con.close()  # Closes out of database
